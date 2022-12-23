2164dbe583962dfa67edfd9103bd300c869b79a4
jjshao <jillian.j.shao@gmail.com>
2022-12-23 15:27:14 -0500

    Add time range functionality

--- R/num.obs.per.tag.R
+++ R/num.obs.per.tag.R
@@ -14,7 +14,12 @@
 #' @param grouping String, grouping name, default to FALSE
 #' @param grouping_col_name String, name of column that includes grouping, default to FALSE, required if grouping!=NULL
 #' @param size_col_name String, name of column that includes size of animal, default to FALSE, required if scatterplot=TRUE
-#' @return Vector of number of total number of observations and total number of tags (for given parameters) and graph if graph=TRUE
+#' @param start_date String, start date in format YYYY-MM-DD, default to NULL
+#' @param end_date String, end date in format YYYY-MM-DD, default to NULL
+#' @param time_interval String, interval of time to separate data into, either "week" or "month", default to NULL
+#' @param time_col_name String, name of column that includes date/time data, default to NULL
+#' @return Vector of number of total number of observations and total number of tags (for given parameters) and graph if graph=TRUE or scatterplot=TRUE,
+#' oor returns integers of number of observations per week/month if date range given
 #' @examples
 #' num_obs_per_tag_lob <- num.obs.per.tag(animal_merged, "Id", graph=TRUE,
 #' sp="Homarus americanus", sp_col_name="Sp")
@@ -26,7 +31,10 @@
 num.obs.per.tag <- function(merged_tags, id_col_name, graph=FALSE, scatterplot=FALSE,
                             metadata=NULL, sp=NULL, sp_col_name=NULL, sex=NULL,
                             sex_col_name=NULL, grouping=NULL, grouping_col_name=NULL,
-                            size_col_name=NULL) {
+                            size_col_name=NULL, start_date=NULL, end_date=NULL,
+                            time_interval=NULL, time_col_name=NULL) {
+
+  # Subset merged_tags by species/sex/grouping
   if (missing(sp) & missing(sex) & missing(grouping)) {
     tags_subset <- merged_tags
   } else if (!missing(sp) & missing(sex) & missing(grouping)) {
@@ -50,6 +58,38 @@ num.obs.per.tag <- function(merged_tags, id_col_name, graph=FALSE, scatterplot=F
   } else {
     stopifnot(!missing(sp_col_name) & !missing(sex_col_name) & !missing(grouping_col_name))
     tags_subset <- subset(merged_tags, merged_tags[, sp_col_name]==sp & merged_tags[, sex_col_name]==sex & merged_tags[, grouping_col_name]) # subset of data this species and sex in this group
+  }
+
+  # Subset tags_subset by dates if provided
+  if (!missing(start_date) | !missing(end_date) | !missing(time_interval)) {
+    # Check to see that we have all the information needed to segment data based on time interval
+    stopifnot(!missing(start_date) & !missing(end_date) & !missing(time_interval) & !missing(time_col_name))
+    stopifnot(time_interval=="week" | time_interval=="month")
+
+    # Ensure that tag data date column are characters
+    tags_subset[, time_col_name] <- as.character(tags_subset[, time_col_name])
+    # Split into date and time
+    tags_subset[, "date"] <- sapply(sapply(tags_subset[, time_col_name],function(x)strsplit(x," ")),function(x)x[1])
+    tags_subset[, "time"] <- sapply(sapply(tags_subset[, time_col_name],function(x)strsplit(x," ")),function(x)x[2])
+    # Change to be in R Date
+    tags_subset[, "date"] <- as.Date(tags_subset[, "date"],format='%Y-%m-%d')
+    # Subset of data within the time interval
+    tags_subset <- subset(tags_subset,
+                          tags_subset[, "date"] < as.Date(end_date) | tags_subset[, "date"] > as.Date(start_date))
+
+    if (time_interval=="week") {
+      # Add column to show week number
+      tags_subset[,"week_num"] <- strftime(tags_subset[, "date"], format = "%V")
+      # Count number of times these occur
+      num_obs_per_time <- table(tags_subset[, "week_num"])
+      return(num_obs_per_time)
+    } else {
+      # Add column for month
+      tags_subset[,"month_num"] <- format(as.Date(tags_subset[, "date"], format="%Y-%m-%d"),"%m")
+      # Count number of times these occur
+      num_obs_per_time <- table(tags_subset[, "month_num"])
+      return(num_obs_per_time)
+    }
   }
 
   num_obs <- length(tags_subset[, id_col_name]) # total number of observations
