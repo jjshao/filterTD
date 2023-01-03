2f9ff2f9d7d3b04b665c8509f7319ec49f1196f8
jjshao <jillian.j.shao@gmail.com>
2023-01-03 14:59:02 -0500

    Update sync.summary

--- R/sync.summary.R
+++ R/sync.summary.R
@@ -1,35 +1,34 @@
-#' Get summary stats for sync tags at chosen HPE levels
+#' Get summary statistics for sync tags at chosen HPE values.
+#' Summary statistics includes mean, maximum, minimum, standard deviation, and standard error.
 #'
 #' @param sync_tags Table of data of sync tags
-#' @param hpe_levels Vector of HPE levels
-#' @return Vector of numerics of summary statistics for sync data, order: (mean, maximum, minimum, standard deviation, standard error)
-#' @examples
-#' num_obs_per_tag_lob <- num.obs.per.tag(animal_merged, "Id", graph=TRUE,
-#' sp="Homarus americanus", sp_col_name="Sp")
+#' @param hpe_col_name String name of column in sync_tags that has HPE values
+#' @param hpe_levels Vector of HPE values
+#' @return Dataframe of numerics of summary statistics for sync data
 #'
 #' @export
 
-sync.summary <- function(sync_tags, hpe_levels) {
-  # table
+sync.summary <- function(sync_tags, hpe_col_name, hpe_levels) {
+  # Create dataframe
   df <- data.frame(matrix(ncol = 6, nrow = 0))
 
+  # Loop through vector of HPE values
   for (x in hpe_levels) {
-    #If we choose HPE <= 25
     sync1 <- subset(sync_tag_data, HPE<=x)
-    # create HPE bins
-    sync1$HPEbin <- as.factor(round(sync1$HPE))
-    avg <- mean(sync1$HPE) # *y value = X m
-    maximum <- max(sync1$HPE) # *y value = X m
-    minimum <- min(sync1$HPE) # *y value = X m
-    std.dev <- sd(sync1$HPE) # *y value = X m
-    std.err <- sd(sync1$HPE) / sqrt(length(sync1$HPE))
-    # vector
+    # Create HPE bins
+    sync1$HPEbin <- as.factor(round(sync1[, hpe_col_name]))
+    avg <- mean(sync1[, hpe_col_name])
+    maximum <- max(sync1[, hpe_col_name])
+    minimum <- min(sync1[, hpe_col_name])
+    std.dev <- sd(sync1[, hpe_col_name])
+    std.err <- sd(sync1[, hpe_col_name])/sqrt(length(sync1[, hpe_col_name]))
+    # Vector of HPE value and statistics
     v <- c(x, avg, maximum, minimum, std.dev, std.err)
-    print(v)
-    # add row to table
+    # Add vector as row to dataframe
     df <- rbind(df,v)
   }
 
+  # Rename dataframe column names
   colnames(df) <- c("HPE", "mean", "maximum", "minimum", "standard deviation", "standard error")
 
   return(df)
