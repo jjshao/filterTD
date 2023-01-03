67d5e3261f7e012c302e968d70dfacf9760dbc0b
jjshao <jillian.j.shao@gmail.com>
2023-01-03 14:58:32 -0500

    Update comments and Rd

--- R/hpe.quantiles.R
+++ R/hpe.quantiles.R
@@ -1,12 +1,12 @@
-#' Get HPE quantiles
+#' Get HPE quantiles corresponding to the given or default probabilities.
 #'
 #' @param sync_tags Table of data of sync tags
 #' @param hpe_col_name String name of column in sync_tags that has HPE values
-#' @param quantiles Default to NULL
-#' @return
+#' @param quantiles Vector of numerics, maximum 6 doubles, default to NULL. When NULL, quantiles = c(0.95, 0.9, 0.75, 0.5, 0.25)
+#' @return Vector of HPE values to cut off at for each quantile.
 #' @examples
-#' num_obs_per_tag_lob <- num.obs.per.tag(animal_merged, "Id", graph=TRUE,
-#' sp="Homarus americanus", sp_col_name="Sp")
+#' quantiles <- c(0.9, 0.8, 0.5, 0.3, 0.1, 0)
+#' sync_hpe2 <- hpe.quantiles(sync_tag_data, "HPE", quantiles)
 #'
 #' @export
 
@@ -15,20 +15,21 @@ hpe.quantiles <- function(sync_tags, hpe_col_name, quantiles=NULL) {
   if (missing(quantiles)) {
     quantiles <- c(0.95, 0.9, 0.75, 0.5, 0.25)
   } else {
+    # Check to make sure vector of quantiles is no longer than 6 doubles
     len_quantiles <- length(quantiles)
     stopifnot(len_quantiles<=6)
+    stopifnot(typeof(quantiles)=="double")
   }
 
   # Create HPE bins, save in new column in sync_tag_data
   sync_tags$HPEbin <- as.factor(round(sync_tags[, hpe_col_name]))
-
   # Count number of detections per HPE value
   bins <- aggregate(HPEm ~ HPEbin, sync_tags, length)
 
   # Adjust parameters of graph
   par(mfrow = c(2,3))
 
-  #loop.filter(sync_tags, quantiles)
+  # Loop through vector of quantiles
   for (x in quantiles) {
     sync_prob <- as.data.frame(with(sync_tags, (tapply(HPEm, HPEbin,
                                                        quantile, probs=x,
@@ -42,10 +43,13 @@ hpe.quantiles <- function(sync_tags, hpe_col_name, quantiles=NULL) {
     # Merge two datasets
     sync_ALL <- merge(bins, sync_prob, by="HPEbin")
 
+    # Plot
     plot(as.numeric(sync_ALL[,1]), as.numeric(sync_ALL[,3]), main="HPE vs HPEm at Q",
          xlab="HPE", ylab="HPEm", pch=19)
+    # Text for changing quantile, written in margin of each graph
     mtext(text = x, side = 3, adj = 0.95, padj = -1.35, cex = 1)
   }
+
   quantile_data <- quantile(sync_tags[, hpe_col_name], probs = quantiles)
 
   return(quantile_data)
