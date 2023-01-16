bbb1c7545421a6f55a35ead7d35f033dd1b8c57e
jjshao <jillian.j.shao@gmail.com>
2023-01-16 16:09:22 -0500

    Adding linear regression to hpe.quantiles

--- R/hpe.quantiles.R
+++ R/hpe.quantiles.R
@@ -35,19 +35,34 @@ hpe.quantiles <- function(sync_tags, hpe_col_name, quantiles=NULL) {
                                                        quantile, probs=x,
                                                        na.rm=TRUE))))
 
-    sync_prob <- as.data.frame(sync_prob)
+    #sync_prob <- as.data.frame(sync_prob)
+    # Rename column
+    colnames(sync_prob) <- c("Q95")
 
     # Create a HPEbin column to merge
     sync_prob$HPEbin <- row.names(sync_prob)
 
     # Merge two datasets
     sync_ALL <- merge(bins, sync_prob, by="HPEbin")
+    print(head(sync_ALL))
+    # Rename
+    sync1_ALL <- sync_ALL
+    print(head(sync1_ALL))
+    #sync1_ALL <- sync1_ALL %>% rename(Q95 = sync_prob)
+    mod <- lm(Q95~as.numeric(HPEbin)-1, data=sync1_ALL)
+    print(summary(mod))
 
-    # Plot
-    plot(as.numeric(sync_ALL[,1]), as.numeric(sync_ALL[,3]), main="HPE vs HPEm at Q",
-         xlab="HPE", ylab="HPEm", pch=19)
-    # Text for changing quantile, written in margin of each graph
-    mtext(text = x, side = 3, adj = 0.95, padj = -1.35, cex = 1)
+    ggplot(sync1_ALL, aes(as.numeric(HPEbin), y=Q95))+
+      geom_point()+ labs(c(x="Binned HPE", y = "95th quantile HPEm"))+ theme_bw()+
+      geom_smooth(method="lm", formula=y~0+x)+
+      annotate(label = sprintf("y = %.3f x\nR? = %.2f", coef(mod), summary(mod)$r.squared),
+               geom = "text", x = 5, y = 30, size = 6)
+
+    # # Plot
+    # plot(as.numeric(sync_ALL[,1]), as.numeric(sync_ALL[,3]), main="HPE vs HPEm at Q",
+    #      xlab="HPE", ylab="HPEm", pch=19)
+    # # Text for changing quantile, written in margin of each graph
+    # mtext(text = x, side = 3, adj = 0.95, padj = -1.35, cex = 1)
   }
 
   quantile_data <- quantile(sync_tags[, hpe_col_name], probs = quantiles)
