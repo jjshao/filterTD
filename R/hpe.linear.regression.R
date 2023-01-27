cb5034ea33132cbd9464d6b94a65150e5fc2aa42
jjshao <jillian.j.shao@gmail.com>
2023-01-27 12:10:29 -0500

    Complete hpe.linear.regression function and start removed.points function

--- R/hpe.linear.regression.R
+++ R/hpe.linear.regression.R
@@ -5,7 +5,7 @@
 #' @param hpem_col_name String name of column in sync_tags that has HPEm values
 #' @param probability
 #' @param hpem_min
-#' @return Vector of HPE values to cut off at for each quantile.
+#' @return Vector of three values (p-value, r-squared, and percentage of removed data).
 #' @examples
 #'
 #' @export
@@ -23,15 +23,15 @@ hpe.linear.regression <- function(sync_tags, hpe_col_name, hpem_col_name,
 
   if(missing(probability) & !missing(hpem_min)) {
     # hpem <= 2 means lose all data points that has hpem > 2
-    sync_prob <- subset(sync_tags, sync_tags[, "HPEm"]<=hpem_min)
-
+    sync_tags$prob <- sync_tags[, hpem_col_name]
+    sync_prob <- subset(sync_tags, sync_tags[, "prob"]<=hpem_min)
   } else if(!missing(probability) & missing(hpem_min)) {
     # probability 0.1 means willing to lose 10% of data
     sync_prob <- as.data.frame(with(sync_tags, (tapply(HPEm, HPEbin,
                                                        quantile, probs=probability,
                                                        na.rm=TRUE))))
     # Rename column
-    colnames(sync_prob) <- c("Q95")
+    colnames(sync_prob) <- c("prob")
   }
 
   # percent remaining
@@ -42,17 +42,17 @@ hpe.linear.regression <- function(sync_tags, hpe_col_name, hpem_col_name,
 
   # Merge two datasets
   sync_ALL <- merge(bins, sync_prob, by="HPEbin")
-  # Rename
-  #sync1_ALL <- sync_ALL
-  #sync1_ALL <- sync1_ALL %>% rename(Q95 = sync_prob)
-  mod <- lm(Q95~as.numeric(HPEbin)-1, data=sync_ALL)
-  print(summary(mod))
 
-  print(ggplot(sync_ALL, aes(as.numeric(HPEbin), y=Q95))+
-    geom_point()+ labs(c(x="Binned HPE", y = "95th quantile HPEm"))+ theme_bw()+
+  # Linear regression of HPEm (prob) and HPEbin
+  mod <- lm(prob~as.numeric(HPEbin)-1, data=sync_ALL)
+
+  print(ggplot(sync_ALL, aes(as.numeric(HPEbin), y=prob))+
+    geom_point() + theme_bw() +
     geom_smooth(method="lm", formula=y~0+x)+
-    annotate(label = sprintf("y = %.3f x\nR? = %.2f", coef(mod), summary(mod)$r.squared),
-             geom = "text", x = 5, y = 30, size = 6)
-)
+    annotate(label = sprintf("y = %.3f x\nR = %.2f", coef(mod), summary(mod)$r.squared),
+             geom = "text", x = (max(as.numeric(sync_ALL$HPEbin))/2), y = (max(sync_ALL$prob)/2), size = 6,
+             colour = "red") +
+    labs(y = "HPEm", x = "Binned HPE", title = "Linear Regression of Binned HPE and HPEm"))
 
+  return(c(summary(mod)$coefficients[,4], summary(mod)$r.squared, perc_remain))
 }
---
+++ R/removed.points.R
@@ -0,0 +1,15 @@
+#' A summary of what points are removed given the current filtering strategy.
+#'
+#' @param sync_tags Table of data of sync tags
+#' @param hpe_col_name String name of column in sync_tags that has HPE values
+#' @param hpem_col_name String name of column in sync_tags that has HPEm values
+#' @param probability
+#' @param hpem_min
+#' @return
+#' @examples
+#'
+#' @export
+
+removed.points <- function(sync_tags, hpe_col_name, hpem_col_name,
+                                  probability=NULL, hpem_min=NULL) {
+}
