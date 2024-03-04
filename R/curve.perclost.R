a6a574ecd46a7db1e56877fd8978583b7ef7fb8d
Shao <Jillian.Shao@dfo-mpo.gc.ca>
2024-03-04 09:46:15 -0500

    Create curve.perclost function
    Outputs a curve that shows percentage of data lost when filtering by certain HPE values

---
+++ R/curve.perclost.R
@@ -0,0 +1,43 @@
+#' Outputs a plot that shows the percentage of data remaining after filtering
+#' by each HPE value.
+#'
+#' @param sync_tags Table of data of sync tags
+#' @param hpe_col_name String name of column in sync_tags that has HPE values
+#' @param hpe_val Integer value for max HPE value user wants to display the graph to (with the max being the largest HPE value rounded to the nearest integer using base R round, IEC 60559 standard)
+#' @examples
+#' # Load data
+#' sync_tag_data <- read.table(file = './extdata/dummy_sync.csv', header=TRUE, sep=",")
+#' # Example
+#' curve.perclost(sync_tags=sync_tag_data, hpe_col_name="HPE", hpe_val=20)
+#'
+#' @export
+
+curve.perclost <- function(sync_tags, hpe_col_name, hpe_val) {
+  # Create HPE bins, save in new column in sync_tag_data
+  sync_tags$HPEbin <- as.factor(round(sync_tags[, hpe_col_name]))
+  sync_tags$HPEm <- sync_tags[, hpem_col_name]
+  # Count number of detections per HPE value
+  bins <- aggregate(HPEm ~ HPEbin, sync_tags, length)
+
+  hpe_vals <- as.numeric(as.character(unique(sync_tags$HPEbin)))
+
+  # Initialize a vector to store the number of points removed for each quantile
+  points_removed <- numeric(length(hpe_vals))
+  # Original number of total points
+  orig_num <- nrow(sync_tags)
+  # Calculate and display the number of points removed for each quantile
+  for (i in seq_along(hpe_vals)) {
+    q <- hpe_vals[i]
+    print(q)
+
+    # Get the indices of points outside the quantile range
+    outside_indices <- which(sync_tags[, hpe_col_name] < q)
+
+    # Get percentage of points removed
+    points_removed[i] <- (orig_num - length(outside_indices))/orig_num
+  }
+
+  result_df <- data.frame(Value = c(hpe_vals),
+                          "Perc_Removed" = points_removed)
+  plot(result_df$Value[1:hpe_val], result_df$Perc_Removed[1:hpe_val])
+}
